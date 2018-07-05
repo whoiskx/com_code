219a43c817db1dfe05879938dff1d96738554ac8
change <574613576@qq.com>
2018-07-05 09:18:14 +0800

    7-5-09

---
+++ my_test/orm/db_test.py
@@ -0,0 +1,19 @@
+# from sqlalchemy import create_engine
+#
+# engine = create_engine()
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = '3306'
+MYSQL_USER = ''
+MYSQL_PASSWORD = ''
+MYSQL_DATABASE = 'info_sct'
+
+import pymysql
+params = {
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = '3306'
+MYSQL_USER = ''
+MYSQL_PASSWORD = ''
+MYSQL_DATABASE = 'info_sct'
+
+}
+db_fb = pymysql.Connect(params)
