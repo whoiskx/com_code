bf3294f0bbef1f10c15375a7fc5d555194dfb747
jianghui <574613576@qq.com>
2018-09-20 10:50:04 +0800

    完成微信自检

---
+++ interface/check/self_check.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import datetime
+import json
+
+import requests
+from openpyxl import load_workbook, Workbook
+
+
+class WxCheck(object):
+    def __init__(self):
+        self.read_name = ''
+        self.account_list = []
+        self.url = 'http://60.190.238.178:38010/search/common/weixin/select?sort=Time%20desc&Account={}&rows=20&starttime=20180825&endtime=20180925'
+        self.write_list = []
+
+    def read(self):
+        wb = load_workbook(self.read_name)
+        sheet = wb.active
+        for row in sheet.rows:
+            account = row[0].value
+            name = row[1].value
+            self.account_list.append([account, name])
+
+    def write(self):
+        for account in self.account_list:
+            url = self.url.format(account[0])
+            resp = requests.get(url)
+            result = json.loads(resp.text)
+            info = result.get('results')
+            if info:
+                print(info[0].get('AddOn'))
+                article_time = info[0].get('AddOn')
+                article_date = datetime.datetime.fromtimestamp(int(article_time[:-3])).date()
+                print(article_date)
+                account.append(str(article_date))
+                self.write_list.append(account)
+            else:
+                print("该url底层无数据: ", url)
+            break
+
+    def create_wb(self):
+        wb = Workbook()
+        sheet = wb.active
+        for account in self.write_list:
+            sheet.append(account)
+        wb.save('result.xlsx')
+
+    def run(self):
+        self.read()
+        self.write()
+        self.create_wb()
+
+
+if __name__ == '__main__':
+    test = WxCheck()
+    test.read_name = 'account.xlsx'
+    test.run()
