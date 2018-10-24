f5e283a349ca0bc2ddbe55a568188bed0da17c6d
jianghui <574613576@qq.com>
2018-10-24 12:17:18 +0800

    '每日自检添加发文时间'

--- wechat/daily/daily_check/self_check.py
+++ wechat/daily/daily_check/self_check.py
@@ -10,7 +10,7 @@ class WxCheck(object):
     def __init__(self):
         self.read_name = ''
         self.account_list = []
-        self.url = 'http://60.190.238.178:38010/search/common/weixin/select?sort=Time%20desc&Account={}&rows=20&starttime=20180825&endtime=20181030'
+        self.url = 'http://60.190.238.178:38010/search/common/weixin/select?sort=Time%20desc&Account={}&rows=20&starttime=20180825&endtime=20181130'
         self.write_list = []
         self.save_name = '{}.xlsx'.format(str(datetime.datetime.today().date()))
 
@@ -29,9 +29,19 @@ class WxCheck(object):
             result = json.loads(resp.text)
             info = result.get('results')
             if info:
-                print(info[0].get('AddOn'))
-                article_time = info[0].get('AddOn')
-                article_date = datetime.datetime.fromtimestamp(int(article_time[:-3])).date()
+                print(account)
+                # 采集时间
+                addon = info[0].get('AddOn')
+                print(addon)
+                # article_time = info[0].get('AddOn')
+                addon_date = datetime.datetime.fromtimestamp(int(addon[:-3])).date()
+                print(addon_date)
+                account.append(str(addon_date))
+                # 发文时间
+                print(info[0].get('Time'))
+                article_time = info[0].get('Time')
+                # article_time = info[0].get('AddOn')
+                article_date = datetime.datetime.fromtimestamp(int(article_time[:-3])).strftime('%Y-%m-%d %H:%M:%S')
                 print(article_date)
                 account.append(str(article_date))
                 self.write_list.append(account)
@@ -43,6 +53,11 @@ class WxCheck(object):
     def create_wb(self):
         wb = Workbook()
         sheet = wb.active
+        sheet.append(['微信ID', '', '最近采集时间', '最近发文时间'])
+        sheet.column_dimensions['A'].width = 15
+        sheet.column_dimensions['C'].width = 15
+        sheet.column_dimensions['D'].width = 25
+
         for account in self.write_list:
             sheet.append(account)
         wb.save(self.save_name)
